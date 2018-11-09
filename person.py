363a02c675dd604f2f44ed2a07dbbaa234625be7
DemineyDev <demineydev@gmail.com>
2018-11-09 17:54:47 +0200

    Committing further exercises

---
+++ person.py
@@ -0,0 +1,9 @@
+user = {
+    'first_name' : 'Steve',
+    'last_name' : 'Jones',
+    'age' : '42',
+    'city' : 'Brooklyn'
+    }
+
+for key, value in user.items():
+    print(key + " is " + value)
