bc0f0d9b378cc80bc752adfccbb85e880b5d93dd
DemineyDev <demineydev@gmail.com>
2018-11-09 16:33:58 +0200

    Committing further changes

---
+++ pizza.py
@@ -0,0 +1,9 @@
+pizzas = ['Pepperoni', "Hawaiian", "Regina", "Garlic"]
+
+for pizza in pizzas:
+    print(pizza)
+
+for pizza in pizzas:
+    print("I like " + pizza + " pizzas")
+
+print("I really like pizza's")
